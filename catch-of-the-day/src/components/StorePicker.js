2bb1d8744e5c1f30f219c30c339f5cdb175257bf
ShubhKakkar <86162630+ShubhKakkar@users.noreply.github.com>
2021-08-01 22:45:30 +0530

    helper functions

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 function StorePicker() {
     return (
         <div>
             <form className="store-selector">
                 <h2>Please Enter A Store</h2>
-                <input type="text" placeholder="Store Name" />
+                <input type="text" placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit">Visit Store ➡ </button>
             </form>
         </div>
